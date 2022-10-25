5695373272aea92f527515f248e90a912eccc06e
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-10-25 14:16:36 +0200

    Added a Django full stack and a MERN CRUD

---
+++ FullStack/CraftingastunningCRUDapplicationwithMERNstack/server/models/models.js
@@ -0,0 +1,20 @@
+// models.js
+const mongoose = require('mongoose');
+
+const TodoListSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+const Todo = mongoose.model('todo', TodoListSchema);
+
+module.exports = Todo;
